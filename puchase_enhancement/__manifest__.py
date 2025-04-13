7afec371df4c0b56c1ddd8d53581da8179908b88
powerfulman86 <dba.ahmedmaher@gmail.com>
2025-04-13 09:23:49 +0200

    modules authorities

--- puchase_enhancement/__manifest__.py
+++ puchase_enhancement/__manifest__.py
@@ -1,35 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "puchase_enhancement",
+    'name': "Purchase Enhancement",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Purchase Enhancement""",
 
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    'author': "CubicIt",
+    'category': 'Inventory/Purchase',
+    'version': '16.0',
+    'license': 'AGPL-3',
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['purchase'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir_rule.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
--- sale_enhancement/__manifest__.py
+++ sale_enhancement/__manifest__.py
@@ -1,35 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sale_enhancement",
+    'name': "Sales Enhancement",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Sales Enhancement""",
 
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    'author': "CubicIt",
+    'category': 'Sales/Sales',
+    'version': '16.0',
+    'license': 'AGPL-3',
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir_rule.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
